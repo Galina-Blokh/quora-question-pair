1617abbeeedc195c4422826367effd295bf25916
mariia_padalko <mary.padalko@gmail.com>
2020-05-18 10:28:45 +0300

    added columns names as arguments to functions

--- notebooks/maria/word_usage_count_func_mp.py
+++ notebooks/maria/word_usage_count_func_mp.py
@@ -2,18 +2,21 @@ from collections import Counter
 import pandas as pd
 import time
 
-def word_usage_count(df, ignore_register = False):
+
+def word_usage_count(df, column1, column2, ignore_register = False):
     """
     Receives a df with raw sentences and counts word usage
     Requires collections.Counter
     Args:
         df: dataframe with some questions
+        column1: columns to unite and count word usage
+        column2: columns to unite and count word usage
         ignore_register: boolean parameter to ignore register or not, defaults to False
     Returns:
         Counter object {word: number}.
     """
-    questions1 = set(df.question1)
-    questions2 = set(df.question2)
+    questions1 = set(df[column1])
+    questions2 = set(df[column2])
     unique_question_union = questions1.union(questions2)
     words_usage_count = Counter()
     for q in unique_question_union:
@@ -24,25 +27,26 @@ def word_usage_count(df, ignore_register = False):
                 words_usage_count[word]+= 1
     return words_usage_count
 
-
-def word_existence_in_corpus(df, ignore_register=False):
+def word_existence_in_corpus(df,column1, column2, ignore_register=False):
     """
     Receives a df with raw sentences and counts word usage
     Requires collections.Counter
     Args:
         df: dataframe with some questions
+        column1: columns to unite and get words
+        column2: columns to unite and get words
         ignore_register: boolean parameter to ignore register or not, defaults to False
     Returns:
         set of words occured in corpus from df.
     """
-    return set(word_usage_count(df, ignore_register = ignore_register).elements())
+    return set(word_usage_count(df, column1, column2, ignore_register = ignore_register).elements())
 
 
 df = pd.read_csv('train_dup.csv').drop(columns='id', axis=1)
 df = df.dropna()
 t0 = time.time()
-total_word_dict = word_usage_count(df)
+total_word_dict = word_usage_count(df,'question1', 'question2',)
 t1 = time.time()
 print(len(total_word_dict))
 print(t1 - t0)
-print(len(word_existence_in_corpus(df)))
+print(len(word_existence_in_corpus(df,'question1', 'question2',)))
